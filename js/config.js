6a343dbcaa4f83efffd8af6d76b9c5d2da5f9954
prachisankhala1003 <prachisankhala@gmail.com>
2025-05-03 16:42:52 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-1_Y1wZszeZq
-        userPoolClientId: '', // e.g. i95j4iacadtph2tnilsqqphl9
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_Y1wZszeZq', // e.g. us-east-1_Y1wZszeZq
+        userPoolClientId: 'i95j4iacadtph2tnilsqqphl9', // e.g. i95j4iacadtph2tnilsqqphl9
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
